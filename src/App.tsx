3a89e869ea7632a08f92c32d20bdf7220fba9ce7
Andy Chen <andy02chen@gmail.com>
2025-06-28 22:12:47 +1000

    mobile fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import Rates from "./components/Rates"
 
 function App() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center 
-    bg-radial-[at_50%_75%] from-sky-200 via-blue-400 to-indigo-900 to-90% text-gray-800 ">
-      <div className="rounded-2xl overflow-auto max-h-[95vh] sm:max-h-[80vh] w-5/6 max-w-[75w] select-none m">
-        <Rates/>
+    <div className="min-h-screen w-screen flex items-center justify-center 
+    bg-radial-[at_50%_75%] from-sky-200 via-blue-400 to-indigo-900 to-90% text-gray-800">
+      <div className="rounded-2xl overflow-auto max-h-[95vh] sm:max-h-[80vh] w-11/12 sm:w-5/6 max-w-[95vw] sm:max-w-[75vw] select-none">
+        <Rates />
       </div>
     </div>
-  )
+  );
 }
 
 export default App
